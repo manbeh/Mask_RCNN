40a8f4c76bde1656c323f23b144fa6d443ee7cac
manbeh <manojbehera.ctc@gmail.com>
2022-11-26 21:56:58 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1009,7 +1009,7 @@ def build_fpn_mask_graph(rois, feature_maps, image_meta,
                            name='mrcnn_mask_bn4')(x, training=train_bn)
     x = KL.Activation('swish_activation')(x)
 
-    x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="swish"),
+    x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="swish_activation"),
                            name="mrcnn_mask_deconv")(x)
     x = KL.TimeDistributed(KL.Conv2D(num_classes, (1, 1), strides=1, activation="sigmoid"),
                            name="mrcnn_mask")(x)
